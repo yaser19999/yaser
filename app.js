6d6651a971343f038ed9809fb8a0af077e36175a
yaser19999 <151687717+yaser19999@users.noreply.github.com>
2023-12-14 05:59:08 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+/*
+const {scrollHeight,scrollTop} = document.documentElement
+
+
+function per(){
+    number = (scrollTop *100)/(scrollHeight-window.innerHeight)
+    console.log(document.body.scrollTop)
+    
+    
+}
+document.addEventListener("scroll",per)
+
+*/
